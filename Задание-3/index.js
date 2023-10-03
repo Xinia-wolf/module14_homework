0a4a3a6d2868c8ffc1e1c021971f907f7e6439ef
Xinia <neko_1996@mail.ru>
2023-10-03 14:18:31 -0600

    Update Задание-3
    Added the task and fixed indentations

--- Задание-3/index.js
+++ Задание-3/index.js
@@ -1,3 +1,13 @@
+/* Задание 3
+Напишите код приложения, интерфейс которого представляет собой input и кнопку. В input можно ввести любое число. 
+При клике на кнопку происходит следующее:
+
+Если число не попадает в диапазон от 1 до 10 — выводить ниже текст «число вне диапазона от 1 до 10».
+Если число попадает в диапазон от 1 до 10 — сделать запрос c помощью XHR по URL https://picsum.photos/v2/list?limit=10, 
+где get-параметр limit — это введённое число.
+Пример. Если пользователь ввёл 5, то запрос будет вида: https://picsum.photos/v2/list?limit=5.
+После получения данных вывести ниже картинки на экран. */
+
 function returnUrl() {
     const value = document.querySelector(".input").value;
     if (+value >= 1 && +value <= 10) {
@@ -66,5 +76,5 @@ function returnUrl() {
   
   // Вешаем обработчик на кнопку для запроса
   btnNode.addEventListener('click', () => {
-  useRequest(returnUrl(), displayResult);
+    useRequest(returnUrl(), displayResult);
   })
